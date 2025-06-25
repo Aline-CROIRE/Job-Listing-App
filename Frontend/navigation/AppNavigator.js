a0932d8979757d41da45d155de7a0f1d193642ec
Afua Hamissi <lyhamissi@example.com>
2025-06-25 11:35:22 +0200

    integrated register and login API'S in frontend

--- Frontend/navigation/AppNavigator.js
+++ Frontend/navigation/AppNavigator.js
@@ -4,6 +4,7 @@ import SplashScreen from '../screens/SplashScreen';
 import UserTypeSelection from '../screens/UserTypeSelection';
 import LoginScreen from '../screens/LoginScreen'; 
 import RegisterScreen from '../screens/RegisterScreen';
+import HomeScreen from '../screens/TalentHomeScreen';
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigator() {
@@ -13,6 +14,7 @@ export default function AppNavigator() {
       <Stack.Screen name="UserTypeSelection" component={UserTypeSelection} />
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name='Register' component={RegisterScreen}/>
+      <Stack.Screen name='Home' component={HomeScreen}/>
     </Stack.Navigator>
   );
 }
